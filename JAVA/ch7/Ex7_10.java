4b2cf028455a6868574cd867506372968d2999c6
GameDevC <chldustmd234@gmail.com>
2023-11-03 15:06:11 +0900

    자바재활 93 / 171

--- JAVA/ch7/Ex7_10.java
+++ JAVA/ch7/Ex7_10.java
@@ -11,6 +11,8 @@ class Ex7_10 {
 
 abstract class Unit {
 	int x, y;			// 유닛의 위치(x, y)
+    int hitpoint;
+    int MAX_HP;
     abstract void move(int x, int y);
     void stop() { /* 현재 위치에 정지 */}
 }
@@ -22,17 +24,37 @@ class Marine extends Unit {
     void stimPack() { /* 스팀팩을 사용한다. */}
 }
 
-class Tank extends Unit {
+class Tank extends Unit implements Repairable {
     void move(int x, int y) {
         System.out.println("Tank[x = " + x + ", y = " + y + "]");
     }
     void changeMode() { /* 공격모드를 변환한다. */}
+    public void Repair(Repairable r){
+        if(r instanceof Unit){
+            Unit u = (Unit)r;
+            while(u.hitpoint != u.MAX_HP){
+                u.hitpoint++;
+            }
+        }
+    }
 }
 
-class DropShip extends Unit {
+class DropShip extends Unit implements Repairable {
     void move(int x, int y) {
         System.out.println("DropShip[x = " + x + ", y = " + y + "]");
     }
     void load() { /* 선택된 대상을 태운다. */}
     void unload() { /* 선택된 대상을 내린다. */}
+    public void Repair(Repairable r){
+        if(r instanceof Unit){
+            Unit u = (Unit)r;
+            while(u.hitpoint != u.MAX_HP){
+                u.hitpoint++;
+            }
+        }
+    }
+}
+
+interface Repairable {
+    void Repair(Repairable r);
 }
