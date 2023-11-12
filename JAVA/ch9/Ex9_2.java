4d9157ca4045d74dc8edb91368336c6bb228c2a5
GameDevC <chldustmd234@gmail.com>
2023-11-12 23:00:20 +0900

    자바재활 104 / 171

---
+++ JAVA/ch9/Ex9_2.java
@@ -0,0 +1,37 @@
+package JAVA.ch9;
+
+class Person {
+    long id;
+
+    public boolean equals(Object obj) {
+        if(!(obj instanceof Person) )
+            return false;
+        
+        Person p = (Person)obj;
+
+        return this.id == p.id;
+    }
+
+    Person(long id) {
+        this.id = id;
+    }
+}
+
+public class Ex9_2 {
+    public static void main(String[] args) {
+        Person p1 = new Person(8011081111222L);
+        Person p2 = new Person(8011081111222L);
+
+        if(p1 == p2) // 주소 비교
+            System.out.println("p1과 p2는 같은 사람입니다.");
+        else
+            System.out.println("p1과 p2는 다른 사람입니다.");
+
+        if(p1.equals(p2)) // Person.id 비교
+            System.out.println("p1과 p2는 같은 사람입니다.");
+        else
+            System.out.println("p1과 p2는 다른 사람입니다.");
+    
+    }
+    
+}
