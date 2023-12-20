6c12ad9a58923908075c228158c728c11d9a88f7
GameDevC <chldustmd234@gmail.com>
2023-12-20 17:48:15 +0900

    자바재활 150 / 171

---
+++ JAVA/ch13/Ex13_3.java
@@ -0,0 +1,27 @@
+package JAVA.ch13;
+
+public class Ex13_3 {
+    static long startTime = 0;
+
+    public static void main(String args[]) {
+        ThreadEx3_1 th1 = new ThreadEx3_1();
+        th1.start();
+        startTime = System.currentTimeMillis();
+
+        for(int i=0; i < 300; i++) {
+            System.out.print("-");
+        }
+
+        System.out.print("소요시간1:" + (System.currentTimeMillis() - Ex13_3.startTime));
+    }
+}
+
+class ThreadEx3_1 extends Thread {
+    public void run() {
+        for(int i=0; i < 300; i++) {
+            System.out.print("|");
+        }
+
+        System.out.print("소요시간2:" + (System.currentTimeMillis() - Ex13_3.startTime));
+    }
+}
